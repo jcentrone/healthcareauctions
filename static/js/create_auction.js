009831b424717ad820935cc1cd8220715c7c8322
Joe Centrone <joecentrone@gmail.com>
2024-07-12 10:46:48 -0400

    Routine Save

--- static/js/create_auction.js
+++ static/js/create_auction.js
@@ -1186,7 +1186,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (isMobileDevice()) {
         document.querySelectorAll('.mobile-only').forEach(function(element) {
-            element.style.display = 'block !important';
+            element.style.display = 'block';
         });
     }
 });
